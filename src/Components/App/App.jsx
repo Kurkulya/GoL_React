f8465ebbd76c9bae3911d3f87920b44de56c7fb0
Kurkulya <kulinich_v@hotmail.com>
2018-02-13 17:04:55 +0200

    initial commit

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss';
+import logo from '../../images/earth.png';
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                    <img className="App-logo" src={logo}/>
+                </header>
+                <p className="App-intro">
+                    To get started, edit and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.jsx?$/,
+                loader: ['react-hot-loader/webpack', 'babel-loader'],
+                exclude: [/node_modules/, /public/]
+            },
+            { test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 20000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "./images/[hash].[ext]"
+                    },
+                },
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
